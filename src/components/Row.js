722f18cf972001716ed0c334c765d07cad4401ef
ANKIT VERMA <ankitvermaksk1234@gmail.com>
2024-01-09 20:13:34 +0530

    new

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,8 @@
 import React,{useState, useEffect} from 'react'
 import axios from "./axios"
 import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
 function Row({title, fetchURL, isLargerow}) {
 
   const baseURL= "https://image.tmdb.org/t/p/original/";
@@ -15,9 +17,31 @@ function Row({title, fetchURL, isLargerow}) {
       fetchdata();
     },[fetchURL]);//this here fetchurl as a conditonal beacuse this variable is used from the outside of the block
     
-    console.log(movies);
+    // console.log(movies);
 
-  
+    const opts= {
+      height: "390",
+      width: "100%",
+      playerVars: {
+
+          autoplay: 1,
+      },
+    };
+
+    const [trailerURL, setTrailurl]= useState("");
+
+  const handleclick= (movie)=>{
+    if(trailerURL){
+      setTrailurl('');
+    }else{
+      movieTrailer(movie?.name || "")
+      .then((url)=>{
+        const urlParams= new URLSearchParams(new URL(url).search);
+        setTrailurl(urlParams.get("v"));
+      })
+      .catch((error)=> console.log(error));
+    }
+  }
   return (
     <div className='row text-white'>
             <h2>{title}</h2>
@@ -26,12 +50,13 @@ function Row({title, fetchURL, isLargerow}) {
               {movies.map(movie=>(
                 <img 
                 key={movie.id}
+                onClick={()=> handleclick(movie)}
                 className={`rowPoster ${isLargerow && "rowPosterlarge"} w-[100%] object-contain max-h-[100px] mr-[10px] hover:scale-105 transition-transform duration-250 ease-in-out cursor-pointer`}
                 src={`${baseURL}${isLargerow?movie.poster_path: movie.backdrop_path}`} 
                 alt={movie.name} />
               ))}
             </div>
-            <YouTube videoId={trailerURL} opts={opts} />
+            {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
     </div>
   )
 }
